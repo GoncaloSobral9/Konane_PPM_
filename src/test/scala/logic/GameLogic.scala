028e9835f70bbee2102ad9b430d57d6f29d41815
Gonçalo Sobral <gfgsl@iscte-iul.pt>
2026-03-26 17:47:15 +0000

    oq fazer no T2

--- src/test/scala/logic/GameLogic.scala
+++ src/test/scala/logic/GameLogic.scala
@@ -10,8 +10,11 @@ object GameLogic {
     (lstOpenCoords(index), newRand)
 
   def play(board:Board, player: Stone, coordFrom: Coord2D, coordTo: Coord2D, lstOpenCoords: List[Coord2D]): (Option[Board], List[Coord2D]) = {
-    val enemy = if(player!=White)White else Black
-
+    val enemy = if player!=Stone.White then White else Black
+    //validar se mov é válido case horiz case vert case None
+    //verificar se salta uma pedra inimiga se calhar fazer recursão caso coma vários
+    //retirar peças enimigas
+    //
     val isValidStart = board.get(coordFrom).contains(player)
     val isDestEmpty = !board.contains(coordTo)
     
