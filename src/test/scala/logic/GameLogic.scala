f90439b81914959ae6265e76989bd83b2b4d1f66
Gonçalo Sobral <gfgsl@iscte-iul.pt>
2026-03-26 15:33:37 +0000

    AAA

--- src/test/scala/logic/GameLogic.scala
+++ src/test/scala/logic/GameLogic.scala
@@ -1,7 +1,8 @@
 package logic
 import model.*
 object GameLogic {
-  def randomMove(lstOpenCoords: List[Coord2D], rand: MyRandom):(Coord2D, MyRandom) ={
-    
+  def randomMove(lstOpenCoords: List[Coord2D], rand: MyRandom):(Coord2D, MyRandom) = {
+    val (index, newRand) = rand.nextInt(lstOpenCoords.length)
+    (lstOpenCoords(index), newRand)
   }
 }
