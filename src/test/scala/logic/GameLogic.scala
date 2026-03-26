1223b578181e1a5cf42c780d94a7ce1cce0329b7
Gonçalo Sobral <gfgsl@iscte-iul.pt>
2026-03-26 16:57:12 +0000

    T1 e começo T2

--- src/test/scala/logic/GameLogic.scala
+++ src/test/scala/logic/GameLogic.scala
@@ -1,9 +1,20 @@
 package logic
+
 import model.*
+import model.Stone.White
+import model.Stone.Black
+
 object GameLogic {
-  def randomMove(lstOpenCoords: List[Coord2D], rand: MyRandom):(Coord2D, MyRandom) = {
+  def randomMove(lstOpenCoords: List[Coord2D], rand: MyRandom): (Coord2D, MyRandom) =
     val (index, newRand) = rand.nextInt(lstOpenCoords.length)
     (lstOpenCoords(index), newRand)
+    
+  def play(board:Board, player: Stone, coordFrom: Coord2D, coordTo: Coord2D, lstOpenCoords: List[Coord2D]): (Option[Board], List[Coord2D]) = {  
+    val enemy = if(player!=White)White else Black
+    
+    val isValidStart = board.get(coordFrom).contains(player)
+    val isDestEmpty = !board.contains(coordTo)
+    
     
   }
 }
--- src/test/scala/model/Board.scala
+++ src/test/scala/model/Board.scala
@@ -1,5 +1,15 @@
 package model
+import scala.collection.parallel.immutable.ParMap
 
 object Board {
+  def initBoard(rows: Int, cols: Int): Board = {
+    ParMap.from(
+      for{
+        r <- 0 until rows
+        c <- 0 until cols
 
+        stone = if ((r + c) % 2 == 0) Stone.Black else Stone.White
+      } yield (r, c) -> stone
+    )
+  }
 }
