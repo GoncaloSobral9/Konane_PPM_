ad9834cedbf3f3ec0db34971871e360aef257556
daniel-masqueiro <dmmos1@iscte-iul.pt>
2026-03-26 19:20:25 +0000

    objeto Board feito

--- src/test/scala/logic/GameLogic.scala
+++ src/test/scala/logic/GameLogic.scala
@@ -9,15 +9,22 @@ object GameLogic {
     val (index, newRand) = rand.nextInt(lstOpenCoords.length)
     (lstOpenCoords(index), newRand)
 
-  def play(board:Board, player: Stone, coordFrom: Coord2D, coordTo: Coord2D, lstOpenCoords: List[Coord2D]): (Option[Board], List[Coord2D]) = {
+  def play(board: Board, player: Stone, coordFrom: Coord2D, coordTo: Coord2D, lstOpenCoords: List[Coord2D]): (Option[Board], List[Coord2D]) = {
     val enemy = if player!=Stone.White then White else Black
-    //validar se mov é válido case horiz case vert case None
-    //verificar se salta uma pedra inimiga se calhar fazer recursão caso coma vários
-    //retirar peças enimigas
-    //
-    val isValidStart = board.get(coordFrom).contains(player)
-    val isDestEmpty = !board.contains(coordTo)
-    
 
+    // 1. Validar se a coordFrom tem uma pedra do 'player'[cite: 59].
+    val isValidStart = board.get(coordFrom).contains(player)
+    // 2. Validar se o movimento é um salto (distância de 2, 4, 6...) sobre pedras inimigas[cite: 47, 48].
+    def getDirection(from: Coord2D, to: Coord2D): Option[(Int,Int)]=
+      (to._1 -from._1, to._2 - from._2) match
+        case ()
+    // 3. Verificar se a coordTo está vazia[cite: 49].
+    val isDestEmpty = !board.contains(coordTo)
+    // 4. Se for válido:
+    //    - Remover a(s) pedra(s) inimiga(s) capturada(s) do ParMap[cite: 49, 50].
+    //    - Mover a pedra do jogador para a coordTo[cite: 59].
+    //    - Atualizar a lista de coordenadas livres (lstOpenCoords)[cite: 60].
+    //    - Retornar (Some(novoTabuleiro), novaListaLivre)[cite: 59, 60].
+    // 5. Se inválido: retornar (None, lstOpenCoords)[cite: 59].
   }
 }
--- src/test/scala/model/Board.scala
+++ src/test/scala/model/Board.scala
@@ -1,15 +1,45 @@
 package model
+
 import scala.collection.parallel.immutable.ParMap
+import scala.collection.parallel.CollectionConverters.*
 
 object Board {
-  def initBoard(rows: Int, cols: Int): Board = {
-    ParMap.from(
-      for{
-        r <- 0 until rows
-        c <- 0 until cols
+  def initFullBoard(rows: Int, cols: Int): Board = {
+    val positions = for {
+      r <- 0 until rows
+      c <- 0 until cols
+      stone = if ((r + c) % 2 == 0) Stone.Black else Stone.White
+    } yield (r, c) -> stone
 
-        stone = if ((r + c) % 2 == 0) Stone.Black else Stone.White
-      } yield (r, c) -> stone
-    )
+    positions.toMap.par
+  }
+
+  def initKonaneBoard(rows: Int, cols: Int, remove1: Coord2D, remove2: Coord2D): (Option[Board], List[Coord2D]) = {
+    val fullBoard = initFullBoard(rows, cols)
+
+    val (r1, c1) = remove1
+    val (r2, c2) = remove2
+
+    def isCorner(r: Int, c: Int): Boolean =
+      (r == 0 || r == rows - 1) && (c == 0 || c == cols - 1)
+
+    def isCenter(r: Int, c: Int): Boolean =
+      (r == rows / 2 || r == rows / 2 - 1) && (c == cols / 2 || c == cols / 2 - 1)
+
+    val isAdjacent = Math.abs(r1 - r2) + Math.abs(c1 - c2) == 1
+
+    val colorsDifferent = (fullBoard.get(remove1), fullBoard.get(remove2)) match {
+      case (Some(s1), Some(s2)) => s1 != s2
+      case _ => false
+    }
+
+    val validPosition = (isCorner(r1, c1) || isCenter(r1, c1)) || (isCorner(r2, c2) || isCenter(r2, c2))
+
+    if (validPosition && colorsDifferent && isAdjacent) {
+      val finalBoard = fullBoard - remove1 - remove2
+      (Some(finalBoard), List(remove1, remove2))
+    } else {
+      (None, Nil)
+    }
   }
 }
